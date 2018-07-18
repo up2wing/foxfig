90704abfe548c6ab0185b2bc8ca6a1fa04701ee6
Yi Wang <up2wing@gmail.com>
2018-07-18 11:35:34 -0400

    [work-config]: git user name and email

--- work_config/work-config.sh
+++ work_config/work-config.sh
@@ -14,7 +14,14 @@ git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.df diff
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-
+read -p "global git user name, just enter to ignore: " git_name
+read -p "global git user mail, just enter to ignore: " git_mail
+if [ ${#git_name} -gt 0 ]; then
+    git config --global user.name "$git_name"
+fi
+if [ ${#git_mail} -gt 0 ]; then
+    git config --global user.email  "$git_mail"
+fi
 
 
 echo "----------fox work config completely------------"
