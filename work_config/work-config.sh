b84d7a0c1980f8f40c8cd6f081f259c69db9fca9
Yi Wang <up2wing@gmail.com>
2018-07-18 10:21:23 -0400

    add work config

---
+++ work_config/work-config.sh
@@ -0,0 +1,19 @@
+#/*===============================================================
+#*   Copyright (C) 2018 All rights reserved.
+#*   
+#*   FileName：work-config.sh
+#*   Author：WangYi
+#*   Date： 2018-07-18
+#*   Description：
+#*
+#================================================================*/
+echo "config git..."
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+
+
+echo "----------fox work config completely------------"
