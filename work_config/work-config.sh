9e4bd71a4503f198e208f8c812ab83ddf772c391
Yi Wang <up2wing@gmail.com>
2022-07-29 23:38:33 -0400

    workconfig: save git token
    Signed-off-by: Yi Wang <up2wing@gmail.com>

--- work_config/work-config.sh
+++ work_config/work-config.sh
@@ -13,6 +13,8 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.df diff
+echo "save token..."
+git config --global credential.helper cache
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 read -p "global git user name, just enter to ignore: " git_name
 read -p "global git user mail, just enter to ignore: " git_mail
