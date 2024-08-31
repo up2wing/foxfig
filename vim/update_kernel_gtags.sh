a1cbbee0378f82883c96b9687af1681d417d0b53
Yi Wang <foxywang@gmail.com>
2024-08-31 21:31:52 +0800

    vim: update
    Signed-off-by: Yi Wang <foxywang@gmail.com>

--- vim/update_kernel_gtags.sh
+++ vim/update_kernel_gtags.sh
@@ -7,16 +7,11 @@
 #*   Description：
 #*
 #================================================================*/
-#产生cscope文件，排除arch非相关目录
 echo will generate gtags for kernel... ...
 gtags_exc="-path ./arch"
-cscope_exc="-path `pwd`/arch"
 find . \( $gtags_exc \) -prune -type f -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.cs' -o -name '*.S' > gtags.files
 find ./arch/x86  -type f -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.cs' -o -name '*.S' >> gtags.files
-find `pwd` \( $cscope_exc \) -prune -type f -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.cs' > cscope.files
-find `pwd`/arch/x86 -type f -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.cs' -o -name '*.S' >> cscope.files
 gtags
-cscope -bkq -i cscope.files
 
 echo gtags built completely!
 
